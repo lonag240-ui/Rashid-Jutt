fdc68586ca83a252d239f3ca784bb21caba56899
lonag240-ui <lonag240@gmail.com>
2026-03-09 15:45:43 +0400

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,17 @@
+const settings = {
+  packname: 'RCJ BOt',
+  author: '‎',
+  botName: "RCJ BOt",
+  botOwner: 'Rashid Jutt', // Your name
+  ownerNumber: '923198583343', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
+gs;
